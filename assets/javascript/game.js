89d9899f6772dbb5f1df5e3045316788589054d4
Maria Wong <maria@Marias-MacBook-Pro.local>
2017-03-21 07:53:35 -0400

    changes theme to aliens

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,7 +9,7 @@ var enemyHP = 0;
 var userChosen;
 var enemyChosen;
 
-//boolean variables to identify when fighters chosen and still in fight area
+//boolean variables to identify when dancers chosen and still in battle area
 var userChar = false;
 var enemy = false;
 
@@ -21,8 +21,8 @@ var originalEnemyHP = 0;
 //counter to count how many enemies chosen to use for reset
 enemyLosses = 0;
 
-//playerDiv used on click instead of player class to retain connection to click 
-//even after moving div to new location during game
+//playerDiv used on click instead of player class to retain connection to click even after moving div 
+//to new location during game
 //userChosen and enemyChosen captures player chosen using player class
 //moves image, DMG and HP of player clicked to userChar or enemy location
 //gets DMG and HP values of player clicked and put in userCharDMG and userCharHP to be modified during game 
@@ -55,7 +55,7 @@ $(document).ready(function() {
 					enemyHP = $(enemyChosen).find('.HP').attr('value');
 					originalEnemyHP = $(enemyChosen).find('.HP').attr('value');
 
-					$("#message").html("Click the attack button to begin the fight.")
+					$("#message").html("Click the dance button to begin the battle.")
 				}
 		}
 
@@ -112,52 +112,91 @@ function replacePlayer(player) {
 	}
 }
 
-//userCharDMG value increases each time click attack and displays the new value
-//hp values decrease each time click attack based on dmg values and displays the new values
+//variables used to animate Gifs
+var state = "";
+var animate = "";
+var still = "";
+
+//function to animate Gif if still and to still image if animated - to be used when click dance
+function animateGif(playerImg) {
+
+	if (enemy == true && userChar == true && userCharHP !== 0) {
+		state = $(playerImg).find("img").attr("data-state");
+		animate = $(playerImg).find("img").attr("data-animate");
+		still = $(playerImg).find("img").attr("data-still");
+
+ 		if(state == 'still') {
+        	$(playerImg).find("img").attr("src", animate);
+        	$(playerImg).find("img").attr("data-state",'animate');
+    	}   
+
+    	if (state == 'animate') {
+       		$(playerImg).find("img").attr("src", still);
+        	$(playerImg).find("img").attr("data-state",'still');
+    	}
+	}
+}
+
+//function to be used when click dance
+//userCharDMG value increases each time click dance and displays the new value
+//hp values decrease each time click dance based on dmg values and displays the new values
 //specifies if (userCharHP > 0 && enemyHP > 0) to prevent calculations after user or enemy loses
 //when user loses display instructions
 //userChar not put at false when userCharHP <= 0 to prevent user from choosing another character before pressing reset
 //when enemy loses, increase enemyLosses, return values to original DMG and HP values and display instructions,
-//replace player (still hidden) and set userChar and enemy to false to allow new fighters to be chosen.
+//replace player (still hidden) and set userChar and enemy to false to allow new dancers to be chosen.
 //specifies (enemy == true && userChar == true) to prevent HP, DMG, and enemyLosses from changing and last players from being
-//hidden (due to replace function) if press attack before choosing new enemy 
-$("#attack").on('click', function () {
+//hidden (due to replace function) if press dance before choosing a new enemy 
+function play() {
+	if (enemy == true && userChar == true) {
 
-if (enemy == true && userChar == true) {
+		if (userCharHP > 0 && enemyHP > 0) {
+			userCharHP -= enemyDMG;
+			enemyHP -= userCharDMG;
+			userCharDMG = parseInt(userCharDMG) + parseInt(originalUserDMG);
+		}
 
-	if (userCharHP > 0 && enemyHP > 0) {
-		userCharHP -= enemyDMG;
-		enemyHP -= userCharDMG;
-		userCharDMG = parseInt(userCharDMG) + parseInt(originalUserDMG);
-	}
+		$(userChosen).find('.DMG').html(userCharDMG);
+		$(userChosen).find('.HP').html(userCharHP);
+		$(enemyChosen).find('.HP').html(enemyHP);
 
-	$(userChosen).find('.DMG').html(userCharDMG);
-	$(userChosen).find('.HP').html(userCharHP);
-	$(enemyChosen).find('.HP').html(enemyHP);
-
-	if (userCharHP <= 0) {
-		$("#message").html("You lost. Please press reset to play again.");
-	}
+		if (userCharHP <= 0) {
+			$("#message").html("You lost. Please press reset to play again.");
+		}
 	
-	if (enemyHP <= 0) {
+		if (enemyHP <= 0) {
   		
-  		$(enemyChosen).find('.HP').html(originalEnemyHP);
+  			$(enemyChosen).find('.HP').html(originalEnemyHP);
   		
 			enemyLosses ++;
 	
  			if (enemyLosses == 3) {
-				$("#message").html("Congratulations. You defeated all enemies. Press reset to play again.");
+				$("#message").html("Congratulations. You defeated all opponents. Press reset to play again.");
  			}
 		 		else {
-					$("#message").html("You won! Choose another enemy to fight.");
+					$("#message").html("You won! Choose another opponent.");
 				} 		
 
-  		replacePlayer(enemyChosen);
+  			replacePlayer(enemyChosen);
 	
-		enemy = false;
+			enemy = false;
 
+		}
 	}
 }
+
+//animates images when click dance for 1.6s, then stills them
+//play function delayed for 1.8s so calculations and replacePlayer 
+//occur after animateGif function complete
+$("#dance").on('click', function () {
+
+	animateGif(userChosen);
+	animateGif(enemyChosen);
+
+	setTimeout(animateGif, 1600, userChosen);
+	setTimeout(animateGif, 1600, enemyChosen);
+
+	setTimeout(play, 1800);
 
 });
 
